51561b5e263566548ef86e0fd25078a7154c6643
ainochi-kor <kthrkdals@gmail.com>
2023-08-27 01:49:33 +0900

    add auth controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,33 @@
+import { authentication, random } from "helpers";
+import { createUser, getUserByEmail } from "../db/users";
+import express, { Request, Response } from "express";
+
+export const register = async (req: Request, res: Response) => {
+  try {
+    const { email, password, username } = req.body;
+
+    if (!email || !password || !username) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+      return res.sendStatus(400);
+    }
+
+    const salt = random();
+    const user = await createUser({
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
+
+    return res.status(200).json(user).end();
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
